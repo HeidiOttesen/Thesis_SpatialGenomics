8366e5893d4239a9267626aad9ab7f5d539459e1
HeidiOttesen <hottesen@tycho.bric.ku.dk>
2022-03-14 11:06:28 +0100

    output+new script for Ves Dev

--- RScripts_Testing/quickload.R
+++ RScripts_Testing/quickload.R
@@ -1,6 +1,6 @@
 library(list)
-files <- list.files(path = '~/R/VesaliusDev/RSource/', pattern = 'R')
-files <- paste0('~/R/VesaliusDev/RSource/',files)
+files <- list.files(path = '~/Thesis_SpatialGenomics/VesaliusDev/RSource/', pattern = 'R')
+files <- paste0('~/Thesis_SpatialGenomics/VesaliusDev/RSource/',files)
 for(i in seq_along(files)){
   source(files[i])
 }
