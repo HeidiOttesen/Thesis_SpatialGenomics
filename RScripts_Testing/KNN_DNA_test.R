e0cd76b0a2dbbcdfd79271b0b30e4e3e449f185a
HeidiOttesen <heidi.ottesen@bric.ku.dk>
2022-03-24 18:12:46 +0100

    Update

--- RScripts_Testing/KNN_DNA_test.R
+++ RScripts_Testing/KNN_DNA_test.R
@@ -13,14 +13,16 @@ Knn(
 
 library(sdpep)
 #Let's make some sample data so people can run it. First a matrix of point coordinates:
-
-sub <- 150
 #coords <- cbind(runif(100), runif(100))
+
+coords <- bead@coordinates
+coords <- cbind(coords$xcoord, coords$ycoord)
+
+#coords <- cbind(ctmp[,2],ctmp[,1])
+#coords <- coords[1:sub, ]
+
 #Then suppose we have A measured at each of the 100 points:
-coords <- cbind(ctmp[,2],ctmp[,1])
-coords <- coords[1:sub, ]
-
-
+# counts / weights to each coordinate:
 #A <- runif(100)
 A <- ctmp[,3]
 a <- A[1:sub]
@@ -30,8 +32,9 @@ a <- A[1:sub]
 #IDs = paste0("ID-",1:100)
 IDs <- bins[1:sub]
 #Then we compute the nearest neighbours (I'm doing 5 nearest for simplicity):
-  
-knn50 <- knn2nb(knearneigh(coords, k = 5), row.names = IDs)
+ctmp.knearneigh <- knearneigh(coords, k = 5)
+
+#knn50 <- knn2nb(knn, row.names = IDs)
 #Each element of knn50 is a vector of the index in coords of the neighbours:
 knn50[[1]]
 
@@ -49,3 +52,22 @@ points(coords[5,,drop=FALSE],col="red",pch=19)
 sapply(1:length(knn50), function(N){mean(A[N])})
 
 #giving the mean of A for the 5 nearest neighbours of each of the 100 points. Note the A value of the point itself isn't included.
+
+
+
+
+
+
+
+
+
+
+# https://github.com/patrickCNMartin/VesaliusDev/blob/main/R/embeddings.R#:~:text=%23%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D%2D-,%23,-%23%20creating%20new%20merged
+
+
+library("dbscan")
+data(iris)
+x <- iris[, -5]
+nn <- kNN(x, k = 5)
+ctmp.knn <- kNN(coords, k = 5)
+
