43196cc7e1038cab6d0c3360ef41929386cda19f
HeidiOttesen <heidi.ottesen@bric.ku.dk>
2022-04-05 13:56:53 +0200

    Update

--- SlideSeq/KNN_DNA_Function.R
+++ SlideSeq/KNN_DNA_Function.R
@@ -1,6 +1,6 @@
 # Copied from KNN_DNA markdown file
 
-KNN.DNA <- function(bead, df, k){
+KNN.DNA.Unique <- function(bead, df, k){
   ## Start
   #- Make sure you have the count df and bead info from the DNA_vesalius markdown..
   #- Barcodes and coordinates from bead_locations
@@ -111,6 +111,124 @@ KNN.DNA <- function(bead, df, k){
   bc.comb <- rownames(comb)
   
 
+  # If reading slideseq bead info from file:
+  #fname <- paste0(in.path, "knn/", alias, "_knn", k)
+  #write.csv(comb, file = paste0(fname, ".bead_locations.csv"), row.names = TRUE, quote = FALSE)
+  #cat(paste(Sys.time()," New bead location file saved as ", paste0(fname, ".bead_locations.csv"), "\n"))
+  #ss <- ReadSlideSeq(paste0(fname, ".bead_locations.csv"))
+  
+  # If creating slideseq bead info as object in list instead:
+  comb <- as.data.frame(comb)
+  ss <- new(Class = 'SlideSeq',assay = "Spatial",
+            coordinates = comb[,c("xcoord","ycoord")])
+  rownames(ss@coordinates) <- rownames(comb)
+  
+  colnames(grMtrx) <- bc.comb
+  rownames(grMtrx) <- bins
+  knnSpMtx <- Matrix(grMtrx, sparse = TRUE)
+  return(list(knnSpMtx, ss))
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+KNN.DNA <- function(bead, df, k){
+  ## Start
+  #- Make sure you have the count df and bead info from the DNA_vesalius markdown..
+  #- Barcodes and coordinates from bead_locations
+  barcodes <- rownames(bead@coordinates)
+  bead.coords <- bead@coordinates
+  coords <- cbind(bead.coords$xcoord, bead.coords$ycoord)
+  l <- length(barcodes)
+  
+  #Perform KNN:
+  cat( paste(Sys.time()," Running K-Nearest Neighbor with k =",k, "number of neighbors", "\n"))
+  coords.knearneigh <- knearneigh(coords, k = k)
+  knn50 <- knn2nb(coords.knearneigh, row.names = barcodes)
+  knn5 <- knn50[1:l] #Just the grouped indexes (to the barcodes) - not the other attributes
+  
+  
+  #Replace barcode index number with barcode string
+  knn.bc <- vector(mode="list", length=l) #holds unique barcodes in groups of up to k
+  i <- 1
+  cat(paste(Sys.time()," Change to barcode names (instead of barcode index numbers)", "\n"))
+  while(i <= l){
+    j <- 1
+    while(j <= k){
+      ix <- knn5[[i]][j]
+      b <- barcodes[ix]
+      knn.bc[[i]][j] <- b
+      j <- j + 1
+    }
+    i <- i + 1
+  }
+  
+  ## Find median coordinate within knn group
+  #- iterate over groups of knn 
+  #- stores their corresponding x and y coordinates in vector t
+  #- Stores the median x and y for each column in new list - comb
+  #- makes a new list of new barcode names, bc.g - the first barcode in each group
+  
+  i <- 1
+  l <- length(knn.bc)
+  comb <- matrix(0, nrow = l, ncol = ncol(bead.coords))
+  bc.g <- vector(length=l)
+  
+  
+  cat(paste(Sys.time()," Finding median coordinate for each knn group", "\n"))
+  while(i <= l){
+    tmp <- knn.bc[[i]]
+    t <- bead.coords[tmp,]
+    ltmp <- length(tmp)
+    comb[i,] <- apply(t, 2, median)
+    if(ltmp > 0){
+      bc.g[[i]] <- tmp[[1]]
+    }
+    i <- i + 1 
+  }
+  
+  rownames(comb) <- bc.g
+  colnames(comb) <- c("xcoord", "ycoord")
+  
+  
+  ## Summing counts over knn-groups:
+  #- Iterate over knn groups 
+  #- take the counts for those barcodes for all bins from the sparsematrix
+  #- df - Sparse matrix vector from the DNA vesalius script - Barcodes as columns, bins as rows.
+  #- Sum the counts for each group and each bin 
+  
+  spMtrx <- df
+  i <- 1
+  
+  l <- length(knn.bc)
+  grMtrx <- matrix(0, nrow = nrow(spMtrx), ncol = l)
+  cat( paste(Sys.time()," Summing the counts for each group and bin", "\n"))
+  while(i <= l){
+    tmp <- knn.bc[[i]]
+    t <- spMtrx[,tmp]
+    ltmp <- length(tmp)
+    if(ltmp > 1){
+      grMtrx[,i] <- rowSums(t)
+    }else if(ltmp == 1){
+      grMtrx[,i] <- t
+    }
+    i <- i + 1 
+  }
+  
+  #Naming rows using new list of barcode names (first barcode of each knn group)
+  bc.comb <- rownames(comb)
+  
+  
   # If reading slideseq bead info from file:
   #fname <- paste0(in.path, "knn/", alias, "_knn", k)
   #write.csv(comb, file = paste0(fname, ".bead_locations.csv"), row.names = TRUE, quote = FALSE)
